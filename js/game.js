18aa55d61887be6c642df94aa8d4168305153594
gin <gin-18@qq.com>
2023-07-10 14:41:05 +0800

    checked right and left collision

--- js/game.js
+++ js/game.js
@@ -46,13 +46,13 @@ class Piece {
       ],
     ];
     this.type = Math.floor(Math.random() * this.shapes.length);
-    this.xOffset = 0;
+    this.xOffset = 3;
     this.yOffset = 0;
   }
 
   drawPiece(piece, map) {
-    let x = this.xOffset / unitBlock;
-    let y = this.yOffset / unitBlock;
+    let x = this.xOffset;
+    let y = this.yOffset;
     for (let n = 0; n < piece.length; n++) {
       for (let m = 0; m < piece[n].length; m++) {
         if (piece[n][m]) {
@@ -62,8 +62,8 @@ class Piece {
     }
   }
   cleanPiece(piece, map) {
-    let x = this.xOffset / unitBlock;
-    let y = this.yOffset / unitBlock;
+    let x = this.xOffset;
+    let y = this.yOffset;
     for (let n = 0; n < piece.length; n++) {
       for (let m = 0; m < piece[n].length; m++) {
         if (piece[n][m]) {
@@ -87,14 +87,24 @@ class Piece {
     }
     return tempPiece;
   }
-  checkBoundary(piece) {
-    let n = piece.length;
-    let m = piece[0].length;
-    for (let i = 0; i < n; i++) {
-      for (let j = 0; j < m; j++) {
-        console.log(piece[i][j]);
-      }
+  checkBoundary(piece, map) {
+    let x = this.xOffset;
+    let y = this.yOffset;
+    // map[y][x + piece[0].length] = 2
+
+    // check collision with left
+    if (map[y][x - 1] === 1 || map[y][x - 1] === undefined) {
+      return true;
     }
+    // check collision with right
+    if (
+      map[y][x + piece[0].length] === 1 ||
+      map[y][x + piece[0].length] === undefined
+    ) {
+      return true;
+    }
+
+    return false
   }
 }
 
@@ -106,6 +116,9 @@ let currentPiece = piece.shapes[piece.type];
 
 piece.drawPiece(currentPiece, game.map);
 
+// piece.checkBoundary(currentPiece, game.map);
+// console.table(game.map);
+
 move();
 
 // test to move
@@ -113,31 +126,35 @@ function move() {
   document.documentElement.addEventListener("keydown", (e) => {
     // move to left
     if (e.keyCode === 37) {
-      piece.checkBoundary(currentPiece);
-      piece.cleanPiece(currentPiece, game.map);
-      piece.xOffset -= unitBlock;
-      piece.drawPiece(currentPiece, game.map);
-      console.table(game.map);
+      if (!piece.checkBoundary(currentPiece, game.map)) {
+        // piece.checkBoundary(currentPiece, game.map);
+        piece.cleanPiece(currentPiece, game.map);
+        piece.xOffset -= 1;
+        piece.drawPiece(currentPiece, game.map);
+        console.table(game.map);
+      }
     }
     // move to right
     if (e.keyCode === 39) {
-      piece.checkBoundary(currentPiece);
-      piece.cleanPiece(currentPiece, game.map);
-      piece.xOffset += unitBlock;
-      piece.drawPiece(currentPiece, game.map);
-      console.table(game.map);
+      if (!piece.checkBoundary(currentPiece, game.map)) {
+        // piece.checkBoundary(currentPiece, game.map);
+        piece.cleanPiece(currentPiece, game.map);
+        piece.xOffset += 1;
+        piece.drawPiece(currentPiece, game.map);
+        console.table(game.map);
+      }
     }
     // move to bottom
     if (e.keyCode === 40) {
-      piece.checkBoundary(currentPiece);
+      piece.checkBoundary(currentPiece, game.map);
       piece.cleanPiece(currentPiece, game.map);
-      piece.yOffset += unitBlock;
+      piece.yOffset += 1;
       piece.drawPiece(currentPiece, game.map);
       console.table(game.map);
     }
     // rotate piece
     if (e.keyCode === 32) {
-      piece.checkBoundary(currentPiece);
+      piece.checkBoundary(currentPiece, game.map);
       piece.cleanPiece(currentPiece, game.map);
       currentPiece = piece.rotatePiece(currentPiece, game.map);
       piece.drawPiece(currentPiece, game.map);
