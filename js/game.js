35c5762c7c32cb2c430738240eb1eed9441471c7
gin <gin-18@qq.com>
2023-07-11 16:17:56 +0800

    render game

--- js/game.js
+++ js/game.js
@@ -7,12 +7,19 @@ canvas.height = 360;
 const unitPiece = 17;
 const unitBlock = 18;
 
-const width = canvas.width / unitBlock;
-const height = canvas.height / unitBlock;
+const width = canvas.width / unitBlock; // col
+const height = canvas.height / unitBlock; // row
 
 class Game {
   constructor() {
     this.map = [...Array(height)].map(() => Array(width).fill("0"));
+  }
+  drawMap() {
+    for (let row = 0; row < this.map.length; row++) {
+      for (let col = 0; col < this.map[row].length; col++) {
+        ctx.fillRect(col * unitBlock, row * unitBlock, unitPiece, unitPiece);
+      }
+    }
   }
 }
 
@@ -56,7 +63,7 @@ class Piece {
     for (let n = 0; n < piece.length; n++) {
       for (let m = 0; m < piece[n].length; m++) {
         if (piece[n][m]) {
-          map[n + y][m + x] = "1";
+          map[n + y][m + x] = 1;
         }
       }
     }
@@ -67,7 +74,7 @@ class Piece {
     for (let n = 0; n < piece.length; n++) {
       for (let m = 0; m < piece[n].length; m++) {
         if (piece[n][m]) {
-          map[n + y][m + x] = "0";
+          map[n + y][m + x] = 0;
         }
       }
     }
@@ -87,24 +94,39 @@ class Piece {
     }
     return tempPiece;
   }
-  checkBoundary(piece, map) {
+  checkHorizontalBoundary(piece, map, direction) {
     let x = this.xOffset;
     let y = this.yOffset;
-    // map[y][x + piece[0].length] = 2
-
     // check collision with left
-    if (map[y][x - 1] === 1 || map[y][x - 1] === undefined) {
+    if (
+      (map[y][x - 1] === 1 || map[y][x - 1] === undefined) &&
+      direction === "left"
+    ) {
       return true;
     }
     // check collision with right
     if (
-      map[y][x + piece[0].length] === 1 ||
-      map[y][x + piece[0].length] === undefined
+      (map[y][x + piece[0].length] === 1 ||
+        map[y][x + piece[0].length] === undefined) &&
+      direction === "right"
     ) {
       return true;
     }
 
-    return false
+    return false;
+  }
+  checkVerticalBoundary(piece, map) {
+    let x = this.xOffset;
+    let y = this.yOffset;
+    // map[y + piece.length][x] = 2;
+    try {
+      if (map[y + piece.length][x] === 1) {
+        return true;
+      }
+    } catch (error) {
+      return true;
+    }
+    return false;
   }
 }
 
@@ -116,49 +138,68 @@ let currentPiece = piece.shapes[piece.type];
 
 piece.drawPiece(currentPiece, game.map);
 
-// piece.checkBoundary(currentPiece, game.map);
-// console.table(game.map);
+render(game.map);
+
+// piece.checkVerticalBoundary(currentPiece, game.map)
+// console.table(game.map)
 
 move();
+
+function render(map) {
+  for (let row = 0; row < map.length; row++) {
+    for (let col = 0; col < map[row].length; col++) {
+      // render map
+      ctx.fillStyle = "black";
+      ctx.fillRect(col * unitBlock, row * unitBlock, unitPiece, unitPiece);
+      // render piece
+      if (map[row][col] === 1) {
+        ctx.fillStyle = "red";
+        ctx.fillRect(col * unitBlock, row * unitBlock, unitPiece, unitPiece);
+      }
+    }
+  }
+}
 
 // test to move
 function move() {
   document.documentElement.addEventListener("keydown", (e) => {
     // move to left
     if (e.keyCode === 37) {
-      if (!piece.checkBoundary(currentPiece, game.map)) {
-        // piece.checkBoundary(currentPiece, game.map);
+      if (!piece.checkHorizontalBoundary(currentPiece, game.map, "left")) {
         piece.cleanPiece(currentPiece, game.map);
         piece.xOffset -= 1;
         piece.drawPiece(currentPiece, game.map);
         console.table(game.map);
+        render(game.map);
       }
     }
     // move to right
     if (e.keyCode === 39) {
-      if (!piece.checkBoundary(currentPiece, game.map)) {
-        // piece.checkBoundary(currentPiece, game.map);
+      if (!piece.checkHorizontalBoundary(currentPiece, game.map, "right")) {
         piece.cleanPiece(currentPiece, game.map);
         piece.xOffset += 1;
         piece.drawPiece(currentPiece, game.map);
         console.table(game.map);
+        render(game.map);
       }
     }
     // move to bottom
     if (e.keyCode === 40) {
-      piece.checkBoundary(currentPiece, game.map);
-      piece.cleanPiece(currentPiece, game.map);
-      piece.yOffset += 1;
-      piece.drawPiece(currentPiece, game.map);
-      console.table(game.map);
+      if (!piece.checkVerticalBoundary(currentPiece, game.map)) {
+        piece.cleanPiece(currentPiece, game.map);
+        piece.yOffset += 1;
+        piece.drawPiece(currentPiece, game.map);
+        console.table(game.map);
+        render(game.map);
+      }
     }
     // rotate piece
     if (e.keyCode === 32) {
-      piece.checkBoundary(currentPiece, game.map);
       piece.cleanPiece(currentPiece, game.map);
       currentPiece = piece.rotatePiece(currentPiece, game.map);
       piece.drawPiece(currentPiece, game.map);
       console.table(game.map);
+      render(game.map);
     }
   });
 }
