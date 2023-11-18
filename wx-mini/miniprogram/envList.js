a6fad5ec373f333998aa1c990b1dcfd6dd1efab1
GIN-18 <gin-18@qq.com>
2023-11-18 14:05:12 +0800

    add wx mini and init project

---
+++ wx-mini/miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-1gztbs8gae199f41","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ wx-mini/uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\software\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-1gztbs8gae199f41 --n quickstartFunctions --r --project "D:\gin\project\javascript\catppuccin-tetris\wx-mini" --report_first --report
