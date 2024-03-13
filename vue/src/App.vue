53da86ec61aa51014ffdeac77bee7e4841cb1de4
gin <gin-18@qq.com>
2024-03-13 19:39:17 +0800

    chore: add forEachShape

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -23,20 +23,27 @@ const level = ref(1);
 
 let gamePlay = ref(false);
 
+let frameId;
 let lastTime = ref(0);
 const frameInterval = ref(1000 / 10);
-
 
 function playGame() {
   gamePlay.value = !gamePlay.value;
 
   addShape();
-  requestAnimationFrame(gameLoop)
+  // gameLoop();
 }
 
 function addShape() {
   currentShape.value = nextShape.value;
   nextShape.value = getShape();
+
+  forEachShape((cs, x, y) => {
+    // game over
+    if (map.value[y] && map.value[y][x]) {
+      cancelAnimationFrame(frameId);
+    }
+  }, 0, 1);
 
   mapCanvas.value.drawShape(currentShape);
   nextCanvas.value.drawShape(nextShape);
@@ -53,33 +60,34 @@ function rotateShape() {
 }
 
 function moveShape(xStep, yStep) {
-  const cs = currentShape.value.pieces[currentShape.value.rotation];
   const w = map.value[0].length;
   const h = map.value.length;
 
   currentShape.value.x += xStep;
   currentShape.value.y += yStep;
 
-  for (let i = 0; i < cs.length; i++) {
-    let x = cs[i][1] + currentShape.value.x;
-    let y = cs[i][0] + currentShape.value.y;
+  forEachShape((cs, x, y) => {
+    for (let i = 0; i < cs.length; i++) {
+      const x = cs[i][1] + currentShape.value.x;
+      const y = cs[i][0] + currentShape.value.y;
 
-    if (xStep && (x < 0 || x >= w || (map.value[y] && map.value[y][x]))) {
-      currentShape.value.x -= xStep;
-    }
+      if (xStep && (x < 0 || x >= w || (map.value[y] && map.value[y][x]))) {
+        currentShape.value.x -= xStep;
+      }
 
-    if(yStep && (y >= h || (map.value[y] && map.value[y][x]))) {
-      currentShape.value.y -= yStep;
-      mergeShape()
-      addShape()
+      if (yStep && (y >= h || (map.value[y] && map.value[y][x]))) {
+        currentShape.value.y -= yStep;
+        mergeShape();
+        addShape();
+      }
     }
-  }
+  });
 
   mapCanvas.value.drawShape(currentShape);
 }
 
 function gameLoop(currentTime) {
-   const deltaTime = currentTime - lastTime.value;
+  const deltaTime = currentTime - lastTime.value;
 
   if (deltaTime > frameInterval.value) {
     // 在这里执行动画逻辑
@@ -87,17 +95,23 @@ function gameLoop(currentTime) {
     lastTime.value = currentTime - (deltaTime % frameInterval.value);
   }
 
-  requestAnimationFrame(gameLoop);
+  frameId = requestAnimationFrame(gameLoop);
 }
 
 function mergeShape() {
+  forEachShape((cs, x, y) => {
+    if (y >= 0) map.value[y][x] = currentShape.value.type + 1;
+  })
+}
+
+function forEachShape(fn, xStep = 0, yStep = 0) {
   const cs = currentShape.value.pieces[currentShape.value.rotation];
 
   for (let i = 0; i < cs.length; i++) {
-    const x = cs[i][1] + currentShape.value.x;
-    const y = cs[i][0] + currentShape.value.y;
+    const x = cs[i][1] + currentShape.value.x + xStep;
+    const y = cs[i][0] + currentShape.value.y + yStep;
 
-    if (y >= 0) map.value[y][x] = currentShape.value.type + 1;
+    fn(cs, x, y);
   }
 }
 
