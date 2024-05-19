203e68139201c217e5eb88f009c3fca1cf1c4113
gin <gin-18@qq.com>
2024-05-19 12:00:13 +0800

    chore: remove jq, set frontend and backend

--- jq/src/js/index.js
+++
@@ -1,8 +0,0 @@
-import '../css/style.css';
-import 'material-icons/iconfont/material-icons.css';
-
-const utils = require('./utils/utils.js');
-
-sessionStorage.removeItem('gameMode');
-
-utils.setPagePaltte();
