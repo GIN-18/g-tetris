18404baf354eafaf699069461fe253876cbe0a7e
gin <gin-18@qq.com>
2024-11-05 10:14:02 +0800

    feat: override check game over in Sprint

--- src/assets/js/mode/Sprint.js
+++ src/assets/js/mode/Sprint.js
@@ -6,7 +6,19 @@ export class Sprint extends Tetris {
     this.lines = 40
   }
 
+  /**
+   * @override 减少行数
+   */
   updateLines() {
     this.lines -= this.getLines()
+  }
+
+  /**
+   * @override 消除满40行游戏结束
+   */
+  checkGameOver() {
+    if (this.matrix[1].some((item) => item > 0) || this.lines <= 0) {
+      return true
+    }
   }
 }
