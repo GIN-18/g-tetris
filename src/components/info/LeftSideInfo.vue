28ebe93bc69298e8e74442b803f17e152d7e02ca
gin <gin-18@qq.com>
2024-11-07 12:09:33 +0800

    refator: left side info for each mode

--- src/components/info/LeftSideInfo.vue
+++ src/components/info/LeftSideInfo.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { inject } from 'vue'
+import { inject, computed } from 'vue'
 import { useRoute } from 'vue-router'
 import { Timer } from '@/assets/js/Timer'
 
@@ -8,9 +8,22 @@ import HoldTetrominoCanvas from '@/components/canvas/HoldTetrominoCanvas.vue'
 
 const route = useRoute()
 const tetris = inject('tetris')
+const mode = route.params.mode
+const minutesSeconds = computed(() =>
+  formatTimeByMode(mode, Timer.formatMinutesSeconds),
+)
+const milliseconds = computed(() =>
+  formatTimeByMode(mode, Timer.formatMilliseconds),
+)
 
-function checkMode(mode) {
-  return route.params.mode === mode
+function formatTimeByMode(mode, formatFn) {
+  if (mode === 'marathon') return
+
+  const time =
+    mode === 'sprint'
+      ? tetris.value.timer.elapsedTime
+      : tetris.value.timer.countdownTime
+  return formatFn(time)
 }
 </script>
 
@@ -20,66 +33,36 @@ function checkMode(mode) {
       <HoldTetrominoCanvas />
     </InfoBox>
 
-    <!-- 限时打分模式显示 -->
-    <div class="flex flex-col gap-6" v-if="checkMode('ultra')">
-      <InfoBox title="LEVEL">
-        <p>{{ tetris.level }}</p>
-      </InfoBox>
+    <div class="flex flex-col gap-6">
+      <!-- 竞速模式不显示 -->
+      <div class="flex flex-col gap-6" v-if="mode !== 'sprint'">
+        <InfoBox title="LEVEL">
+          <p>{{ tetris.level }}</p>
+        </InfoBox>
 
-      <InfoBox title="SCORE">
-        <div class="flex gap-2" v-if="tetris.starCount">
-          <i class="nes-icon is-small star"></i>
-          <p>x</p>
-          <p>{{ tetris.starCount }}</p>
-        </div>
+        <InfoBox title="SCORE">
+          <div class="flex gap-2" v-if="tetris.starCount">
+            <i class="nes-icon is-small star"></i>
+            <p>x</p>
+            <p>{{ tetris.starCount }}</p>
+          </div>
 
-        <div class="flex gap-2" v-if="tetris.coinCount">
-          <i class="nes-icon is-small coin"></i>
-          <p>x</p>
-          <p>{{ tetris.coinCount }}</p>
-        </div>
+          <div class="flex gap-2" v-if="tetris.coinCount">
+            <i class="nes-icon is-small coin"></i>
+            <p>x</p>
+            <p>{{ tetris.coinCount }}</p>
+          </div>
 
-        <p>{{ tetris.score }}</p>
-      </InfoBox>
+          <p>{{ tetris.score }}</p>
+        </InfoBox>
+      </div>
 
-      <InfoBox title="TIMER">
+      <!-- 马拉松模式不显示 -->
+      <InfoBox title="TIMER" v-if="mode !== 'marathon'">
         <div class="flex flex-col justify-center items-end gap-1">
-          <p>{{ Timer.formatMinutesSeconds(tetris.timer.countdownTime) }}</p>
-          <p>{{ Timer.formatMilliseconds(tetris.timer.countdownTime) }}</p>
+          <p>{{ minutesSeconds }}</p>
+          <p>{{ milliseconds }}</p>
         </div>
-      </InfoBox>
-    </div>
-
-    <!-- 竞速模式显示 -->
-    <div class="flex flex-col gap-6" v-if="checkMode('sprint')">
-      <InfoBox title="TIMER">
-        <div class="flex flex-col justify-center items-end gap-1">
-          <p>{{ Timer.formatMinutesSeconds(tetris.timer.elapsedTime) }}</p>
-          <p>{{ Timer.formatMilliseconds(tetris.timer.elapsedTime) }}</p>
-        </div>
-      </InfoBox>
-    </div>
-
-    <!-- 马拉松模式显示 -->
-    <div class="flex flex-col gap-6" v-if="checkMode('marathon')">
-      <InfoBox title="LEVEL">
-        <p>{{ tetris.level }}</p>
-      </InfoBox>
-
-      <InfoBox title="SCORE">
-        <div class="flex gap-2" v-if="tetris.starCount">
-          <i class="nes-icon is-small star"></i>
-          <p>x</p>
-          <p>{{ tetris.starCount }}</p>
-        </div>
-
-        <div class="flex gap-2" v-if="tetris.coinCount">
-          <i class="nes-icon is-small coin"></i>
-          <p>x</p>
-          <p>{{ tetris.coinCount }}</p>
-        </div>
-
-        <p>{{ tetris.score }}</p>
       </InfoBox>
     </div>
   </div>
